c98220f70fd74a04e7840625028d6c6873b89c7f
nick-a-schneider <schneidernick3@gmail.com>
2021-05-12 17:41:03 -0400

    individual joint gains for ds4 control. Launch file verified

--- src/dualshock_teleop.py
+++ src/dualshock_teleop.py
@@ -9,22 +9,27 @@ class Arm_Goal(object):
     def __init__(self, publisher):
 
         self._pub = publisher
-        self.remap = {
-            'axis_left_x':'L',
-            'axis_left_y':'X',
+        self.joints = {
+            'axis_left_x': 'L',
+            'axis_left_y': 'X',
             'axis_right_x':'T',
             'axis_right_y':'P'
+        }
+        self.scale = {
+            'axis_left_x':   0.15,
+            'axis_left_y':   1,
+            'axis_right_x':  1,
+            'axis_right_y':  1
         }
 
     def callback(self, msg):
         PWM_RES = 255
         input_vals = {}
         goal = arm_msg()
-        for attr in self.remap.keys():
+        for attr in self.joints.keys():
             read_val = getattr(msg, attr)
-
-            goal.data = (PWM_RES+1) + PWM_RES*read_val
-            goal.joint = ord(self.remap[attr])
+            goal.data = (PWM_RES+1) + PWM_RES*read_val*self.scale[attr]
+            goal.joint = ord(self.joints[attr])
             self._pub.publish(goal)
 
 
